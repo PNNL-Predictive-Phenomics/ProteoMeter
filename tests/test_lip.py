d6e342c922a17c2506454e612a6d408c124eed16
Jordan Rozum <jordan.rozum@pnnl.gov>
2025-04-21 15:27:09 -0700

    simplify LiP test

--- tests/test_lip.py
+++ tests/test_lip.py
@@ -1,9 +1,16 @@
+import warnings
+
 from proteometer.lip_analysis import lip_analysis
 from proteometer.params import Params
 
 
-def test_lip_analysis():
+def test_lip_simple():
+    warnings.filterwarnings(
+        "error"
+    )  # example constructed to be numerically fine; let's test that
     par = Params("tests/data/test_config_lip.toml")
     df = lip_analysis(par)
-
+    warnings.resetwarnings()
     assert df is not None
+    print(df)
+    print(df.columns)
