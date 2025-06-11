11c7d42db6d1afdf17adcb7070cfc93a4bb94405
Jordan Rozum <jordan.rozum@pnnl.gov>
2025-06-11 12:13:37 -0700

    linting

--- src/proteometer/utils.py
+++ src/proteometer/utils.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from typing import TYPE_CHECKING, Literal, cast
 
 import numpy as np
+
 if TYPE_CHECKING:
     from collections.abc import Sequence
 
@@ -103,9 +104,9 @@ def filter_missingness(
         raise ValueError(f"Unknown method: {method}. Use 'all' or 'any'.")
     return df_w
 
+
 def expsum(x: pd.Series[float]) -> float:
     val = cast(float, np.nansum(2 ** (x.replace(0, np.nan))))
     if val == 0:
         return np.nan
     return np.log2(val)
-
