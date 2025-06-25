6bb2ce08b9daba4be4aad106ed125d188ac3e91b
Jordan Rozum <jordan.rozum@pnnl.gov>
2025-06-25 16:15:16 -0700

    prepare new data location

--- demonstration.py
+++ demonstration.py
@@ -9,20 +9,13 @@ drop_samples = [
     "Mock_24h_1",
 ]
 drop_samples = None
-lip_params = Params("scratch.data/scratch_lip.toml")
+lip_params = Params("demo_data/lip.toml")
 lip_pept, lip_site, lip_prot = lip_analysis(lip_params, drop_samples=drop_samples)
-lip_pept.to_csv("scratch.data/scratch_lip_processed_pept.csv")
-lip_site.to_csv("scratch.data/scratch_lip_processed_site.csv")
-lip_prot.to_csv("scratch.data/scratch_lip_processed_prot.csv")
+lip_pept.to_csv("demo_data/lip_processed_pept.csv")
+lip_site.to_csv("demo_data/lip_processed_site.csv")
+lip_prot.to_csv("demo_data/lip_processed_prot.csv")
 
-
-lip_params = Params("scratch.data/scratch_lip_no_abundance.toml")
-lip_pept, lip_site, lip_prot = lip_analysis(lip_params, drop_samples=drop_samples)
-lip_pept.to_csv("scratch.data/scratch_lip_processed_pept_no_abundance.csv")
-lip_site.to_csv("scratch.data/scratch_lip_processed_site_no_abundance.csv")
-lip_prot.to_csv("scratch.data/scratch_lip_processed_prot_no_abundance.csv")
-
-ptm_params = Params("scratch.data/scratch_ptm.toml")
+ptm_params = Params("demo_data/ptm.toml")
 ptm_site, ptm_prot = ptm_analysis(ptm_params)
-ptm_site.to_csv("scratch.data/scratch_ptm_processed_site.csv")
-ptm_prot.to_csv("scratch.data/scratch_ptm_processed_prot.csv")
+ptm_site.to_csv("demo_data/ptm_processed_site.csv")
+ptm_prot.to_csv("demo_data/ptm_processed_prot.csv")
