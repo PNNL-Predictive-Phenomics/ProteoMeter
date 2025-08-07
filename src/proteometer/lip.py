f217d098f6353fe9a691e6e05ed9defeb1a63b88
Song Feng (馮松) <LifeWorks@users.noreply.github.com>
2025-08-06 17:00:44 -0700

    fixed a bug: when filtering contaminants, 'Protein' column will be copied to 'UniProt' column

--- src/proteometer/lip.py
+++ src/proteometer/lip.py
@@ -24,8 +24,8 @@ if TYPE_CHECKING:
 def filter_contaminants_reverse_pept(
     df: pd.DataFrame,
     search_tool: Literal["maxquant", "msfragger", "fragpipe"],
-    protein_id_col_pept: str,
-    uniprot_col: str,
+    protein_id_col_pept: str = "Protein",
+    uniprot_col: str = "UniProt",
 ) -> pd.DataFrame:
     """
     Filters out contaminants and reverse hits from a peptide DataFrame.
@@ -47,10 +47,10 @@ def filter_contaminants_reverse_pept(
             & (~df[protein_id_col_pept].str.contains("(?i)REV__"))
             & (~df[protein_id_col_pept].str.contains("(?i)CON__"))
         ].copy()
-        df[uniprot_col] = df[protein_id_col_pept].astype(str)
+        # df[uniprot_col] = df[protein_id_col_pept].astype(str)
     elif search_tool.lower() == "msfragger" or search_tool.lower() == "fragpipe":
         df = df[(~df[protein_id_col_pept].str.contains("(?i)contam_"))].copy()
-        df[uniprot_col] = df[protein_id_col_pept].astype(str)
+        # df[uniprot_col] = df[protein_id_col_pept].astype(str)
     else:
         print(
             "The search tool is not specified or not supported yet. "
